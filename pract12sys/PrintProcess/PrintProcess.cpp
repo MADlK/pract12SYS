d2ff139515c8eccd98fd91c6d5080b3081646520
MADlK <m20eryem03@gmail.com>
2026-02-09 11:47:06 +0500

    Add files via upload

---
+++ pract12sys/PrintProcess/PrintProcess.cpp
@@ -0,0 +1,79 @@
+﻿// PrintProcess.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <Ctime>
+#include <thread>
+using namespace std;
+
+DWORD getRandomDelay(int minMs, int maxMs) {
+    return minMs + rand() % (maxMs - minMs + 1);
+}
+
+
+int main()
+{
+    HANDLE hMutex = CreateMutex(nullptr, FALSE, L"mutex");
+    if (!hMutex)
+    {
+        cout << "Err create mutex" << endl;
+        return 0;
+    }
+
+    //10 минут?
+
+    DWORD Out = 60 * 10 * 1000;
+    DWORD start = GetTickCount();
+
+    //сам процесс
+    while (true)
+    {
+
+        //проверка что принтер используется
+        HANDLE hClient = OpenMutex(SYNCHRONIZE, FALSE, L"mutex");
+        if (hClient)
+        {
+            DWORD wait = WaitForSingleObject(hClient, 0);
+            if (wait == WAIT_OBJECT_0)
+            {
+                start = GetTickCount();
+                cout<<"time out 00 min" << endl;
+                ReleaseMutex(hClient);
+            }
+            CloseHandle(hClient);
+        }
+
+
+        
+        
+        
+        DWORD end = GetTickCount();
+        if ((end - start) > 10000)
+        {
+            system("cls");
+            cout << "Print Error" << endl;
+        }
+       
+            system("cls");
+            cout << "Pechataet..." << endl;
+        
+            
+        
+        WaitForSingleObject(hMutex, getRandomDelay(5000, 15000));
+
+         
+        
+    }
+    ReleaseMutex(hMutex);
+    system("cls");
+    cout << "Completed" << endl;
+    
+
+
+    
+
+
+}
+
+
---
+++ pract12sys/pract12/pract12.cpp
@@ -0,0 +1,37 @@
+﻿// pract12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <Ctime>
+#include <thread>
+
+using namespace std;
+
+
+wchar_t print[] = L"C:\\Users\\st310-09\\Desktop\\pract12sys\\pract12\\x64\\Debug\\PrintProcess.exe";
+
+
+int main()
+{
+    
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    DWORD p;
+    if (!CreateProcess((LPWSTR)print, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    {
+        cout<<"Error create" << GetLastError()<<endl;
+        return 0;
+    }
+    
+    
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+    
+
+    
+}
+
+
