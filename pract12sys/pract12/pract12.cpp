e68886b81a5bcf9015dcec931ea27ef998386e8d
MADlK <m20eryem03@gmail.com>
2026-02-27 11:50:23 +0500

    Add files via upload

--- pract12sys/pract12/pract12.cpp
+++ pract12sys/pract12/pract12.cpp
@@ -3,39 +3,52 @@
 
 #include <iostream>
 #include <Windows.h>
-
+#include <Conio.h>
 #include <thread>
 
 using namespace std;
 
 
-//wchar_t print[] = L"C:\\Users\\st310-09\\Desktop\\pract12sys\\pract12\\x64\\Debug\\PrintProcess.exe";
-wchar_t print[] = L"C:\\Users\\m20er\\OneDrive\\Рабочий стол\\pract12sys\\pract12\\x64\\Debug\\PrintProcess.exe";
+
+//wchar_t print[] = L"C:\\Users\\st310-09\\Desktop\\pract12SYS-main\\pract12sys\\pract12\\x64\\Debug\\PrintProcess.exe";
+//wchar_t print[] = L"C:\\Users\\m20er\\OneDrive\\Рабочий стол\\pract12sys\\pract12\\x64\\Debug\\PrintProcess.exe";
 
 
 int main()
 {
     
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&si, sizeof(STARTUPINFO));
-    si.cb = sizeof(STARTUPINFO);
-    DWORD p;
-    if (!CreateProcess(NULL, print, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
-    {
-        cout<<"Error create" << GetLastError()<<endl;
-        return 0;
-    }
+    //STARTUPINFO si;
+    //PROCESS_INFORMATION pi;
+    //ZeroMemory(&si, sizeof(STARTUPINFO));
+    //si.cb = sizeof(STARTUPINFO);
+    //DWORD p;
+    //if (!CreateProcess(NULL, print, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    //{
+    //    cout<<"Error create" << GetLastError()<<endl;
+    //    return 0;
+    //}
    
 
 
     
     
     
-    CloseHandle(pi.hThread);
+ /* 
+ CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
+    */
     
-
+    HANDLE hMutex = CreateMutex(NULL, FALSE, L"mutex");
+    if (!hMutex)
+    {
+        cout << "Err create mutex" << endl;
+        return 0;
+    }
+    WaitForSingleObject(hMutex, INFINITE);
+    
+    CloseHandle(hMutex);
+    Sleep(10000);
+    cin;
     
 }
 
